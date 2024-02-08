4f54b047f7ea0e7f18e2354f4484903a1dc0a8af
Love Tätting <lovetatting@gmail.com>
2024-02-07 17:00:09 -0800

    Took away default of downsample and fixed shellscript to clone the repo with disassembled R files

--- stage/entrypoint.sh
+++ stage/entrypoint.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-# Download the R Markdown file
-Rscript -e "download.file('https://raw.githubusercontent.com/lovetatting/vasaikar_seurat/main/stage/GEOdataVasaikar.Rmd', destfile = '/home/ruser/GEOdataVasaikar.Rmd')"
+# Clone the repository
+git clone git@github.com:lovetatting/vasaikar_seurat.git
+
+# Change directory to Vasaikar_seurat/stage
+cd vasaikar_seurat/stage
 
 # Check if the first argument is 'debug'
 if [ "$1" == "compute" ]; then
     echo "Running in compute mode: Purling and executing R file"
     # Purl the Rmd file into an R script
     export NO_PLOTS=TRUE
-    Rscript -e "knitr::purl('/home/ruser/GEOdataVasaikar.Rmd', output = '/home/ruser/GEOdataVasaikar.R')"
+    Rscript -e "knitr::purl('GEOdataVasaikar.Rmd', output = 'GEOdataVasaikar.R')"
     # Run the purled R script
     Rscript /home/ruser/GEOdataVasaikar.R
 else
     # Render the R Markdown file
-    Rscript -e "rmarkdown::render(input = '/home/ruser/GEOdataVasaikar.Rmd')"
+    Rscript -e "rmarkdown::render(input = 'GEOdataVasaikar.Rmd')"
 fi
 
